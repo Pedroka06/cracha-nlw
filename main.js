d9210996ccdf94f653685446bbc7bb6b8e3cfb5f
Pedro Pereira Baptista <65473628+Pedroka06@users.noreply.github.com>
2021-10-23 12:11:31 -0300

    Coments Initial

--- main.js
+++ main.js
@@ -1,38 +1,7 @@
-// const LinksSocialMedia = {
-//   github: 'pedroka06',
-//   youtube: 'PedroBaptista1000',
-//   facebook: 'pedroka.baptista',
-//   instagram: 'pedroka.oficial06',
-//   twitter: 'pedropbaptista'
-// }
-// // função com for laço de repetição usando variavel para vereficar elemento li filho da ul 
-// function changeSocialMediaLinks() {
-//   for (let li of socialLinks.children) {
-//     const social = li.getAttribute('class');
     
-//     li.children[0].href = `https://${social}.com/${LinksSocialMedia[social]}`
-//   }
-// }
-
-// changeSocialMediaLinks()
-
-
-// function getGitHubProfileInfos() {
-//   const url = `https://api.github.com/users/${LinksSocialMedia.github}`
+ //  const url = `https://api.github.com/users/${LinksSocialMedia.github}`
   
-//   fetch(url)
-//   .then(response => response.json())
-//   .then(data => {
-//     userName.texContent = data.name
-//     userBio.texContent = data.Bio
-//     userLink.href = data.html_url
-//     UserImage.src = data.avatar_url
-//     userLogin.texContent = data.login
-//   })
-// }
-
-// getGitHubProfileInfos()
-
+/* constante com as exteções das Media Sociais */
 const LinksSocialMedia = {
   github: 'Pedroka06',
   youtube: 'PedroBaptista1000',
